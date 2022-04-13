28e50415d7a241e9ff649c6813f1e0e35f225605
sorgeligt <ubutno@gmail.com>
2022-04-14 00:34:29 +0300

    .

--- src/main/java/ru/mail/polis/andreyilchenko/MemorySegmentComparator.java
+++ src/main/java/ru/mail/polis/andreyilchenko/MemorySegmentComparator.java
@@ -15,14 +15,14 @@ public final class MemorySegmentComparator implements Comparator<MemorySegment> 
     @Override
     public int compare(MemorySegment m1, MemorySegment m2) {
         long firstMismatch = m1.mismatch(m2);
-        if (firstMismatch == -1) {
-            return 0;
-        }
         if (firstMismatch == m2.byteSize()) {
             return 1;
         }
         if (firstMismatch == m1.byteSize()) {
             return -1;
+        }
+        if (firstMismatch == -1) {
+            return 0;
         }
         return Byte.compareUnsigned(
                 MemoryAccess.getByteAtOffset(m1, firstMismatch),
